cbfb06bb93c904187fc913091eb198324175de3d
xerrf <beve3854@colorado.edu>
2019-03-18 16:53:21 -0600

    finished the parallax on the inside tunnel and cleaned up some stuff.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 $(document).ready( function() {
     console.log("In main.js");
+
+    // dynamically change the position of the front tunnel so that it is resonsive
+    var centerPX = $(window).width();
+    var centerEdge = centerPX/2 + 92;
+    // console.log(centerEdge);
+
+    $("#tunnel-front-right").css( {
+        "left" :  centerEdge + "px"
+    });
+    $("#tunnel-front-left").css( {
+        "right" : centerEdge + "px"
+    });
+
     $(window).scroll(function() {
         var wScroll = $(this).scrollTop();
-        console.log(wScroll);
+        // console.log(wScroll);
 
+        // this handles the parallax for the first section
         if (wScroll > 600 && wScroll <=2500) {
             $("#spires-back").css( {
                 "transform" : "translate(0px, -" + wScroll/80 + "%)"
-                }
-            );
+            });
             $("#spires-mid1").css( {
                 "transform" : "translate(0px, -" + wScroll/40 + "%)"
             });
@@ -22,6 +35,7 @@ $(document).ready( function() {
                 "transfrom" : "translate(0px, -" + wScroll/10 + "%)"
             });
         }
+        // this handles the extra space left by the front spires moving upwards
         if (wScroll >= 1300) {
             $("#extra-white").css( {
                 "height" : "80vh"
@@ -29,6 +43,27 @@ $(document).ready( function() {
         }
         else $("#extra-white").css( {
             "height" : "0px"
-        })
+        });
+
+        // this handles the parallax in the tunnel
+        if (wScroll > 2000 && wScroll <=5000)
+        $("#tunnel-back-right").css( {
+            "transform" : "translate(0px, -" + wScroll/8 + "px) scale(-1)"
+        });
+        $("#tunnel-back-left").css( {
+            "transform" : "translate(0px, -" + wScroll/8 + "px)"
+        });
+        $("#tunnel-mid1-right").css( {
+            "transform" : "translate(0px, " + wScroll/3 + "px) scale(-1)"
+        });
+        $("#tunnel-mid1-left").css( {
+            "transform" : "translate(0px, " + wScroll/3 + "px)"
+        });
+        $("#tunnel-mid2-right").css( {
+            "transform" : "translate(0px, -" + wScroll/2 + "px) scale(-1)"
+        });
+        $("#tunnel-mid2-left").css( {
+            "transform" : "translate(0px, -" + wScroll/2 + "px)"
+        });
     });
 });
